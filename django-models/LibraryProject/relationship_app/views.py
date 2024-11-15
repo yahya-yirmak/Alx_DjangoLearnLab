1ff5d0bb13520f64f756835626fea946ed423b30
yahya yirmak <yahyayirmak9@gmail.com>
2024-11-15 22:18:24 +0100

    updating views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -26,6 +26,6 @@ class LibraryDetailView(DetailView):
         return context
 
 class register(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
