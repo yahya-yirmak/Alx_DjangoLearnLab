bd057cd6c81b600422263fc7e84aa80d77e9f266
yahya yirmak <yahyayirmak9@gmail.com>
2024-11-15 22:05:22 +0100

    updating views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
 # Create your views here.
 
