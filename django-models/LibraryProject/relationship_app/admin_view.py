0c8d62cb842cc4cdb2aafca75af964bfc4b0d6d2
yahya yirmak <yahyayirmak9@gmail.com>
2024-11-17 18:01:32 +0100

    Updating django-models directory

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -3,8 +3,9 @@ from django.contrib.auth.decorators import user_passes_test
 
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
+
