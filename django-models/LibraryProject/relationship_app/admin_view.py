3148193000982d881ff7a9461c199189e660cdb7
yahya yirmak <yahyayirmak9@gmail.com>
2024-11-17 18:10:17 +0100

    Updating django-models directory

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 # Access Control: Only 'Admin' users can access
+
 def is_admin(user):
-    return user.role == 'Admin'
+    return user.groups.filter(name='Admin').exists()
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
-
+    return render(request, 'relationship_app/admin_view.html')
 
