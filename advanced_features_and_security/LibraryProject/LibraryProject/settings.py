e133b54961ea342a95d40b6d588dfc17f2d8f45e
yahya yirmak <yahyayirmak9@gmail.com>
2024-11-24 15:13:55 +0100

    Updating files inside advanced_features_and_security directory

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'bookshelf/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
