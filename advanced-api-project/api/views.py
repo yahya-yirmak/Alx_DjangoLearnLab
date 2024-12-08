179dac890ca56d0fcdd707d2cc6ca08094442674
yahya yirmak <yahyayirmak9@gmail.com>
2024-12-08 14:44:51 +0100

    Updating advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,6 +6,7 @@ from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import BookFilter
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 
 # Create your views here.
 
