e153d3790a172083420235bbfaf81dfacb5c6933
yahya yirmak <yahyayirmak9@gmail.com>
2024-12-08 23:40:03 +0100

    Adding django_blog directory

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add_comment'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit_comment'),
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='edit_comment'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
     
 ]
