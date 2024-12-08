c693b29ee07e4bf5fdcc8917b821d1f257ea3181
yahya yirmak <yahyayirmak9@gmail.com>
2024-12-08 14:29:33 +0100

    Updating advanced-api-project

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List all books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Detail of a specific book
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a specific book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a specific book
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # Update a specific book
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a specific book
 ]
