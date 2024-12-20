843ad79ff0d2dcecb4d2e0d4cbdb7ea6e040b421
yahya yirmak <yahyayirmak9@gmail.com>
2024-12-20 16:25:38 +0100

    Updating social_media_api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import GenericAPIView
+from accounts.models import CustomUser
 # Create your views here.
 
 
@@ -17,10 +18,21 @@ class CommentViewset(viewsets.ModelViewSet):
     serializer_class =CommentSerializer
 
 
-class FeedView(ListAPIView):
+class FeedView(GenericAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        # Get the current user
         user = self.request.user
-        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
+
+        # Ensure the user is a CustomUser instance
+        following_users = CustomUser.objects.filter(pk__in=user.following.all())
+        
+        # Filter posts by users the current user follows
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
