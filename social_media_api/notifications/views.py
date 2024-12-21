93544dd89660ab9f1090e1bc96e4ed79475e7fbd
yahya yirmak <yahyayirmak9@gmail.com>
2024-12-21 15:31:50 +0100

    Updating social_media_api

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,44 +1,4 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
-from rest_framework.response import Response
-from posts.models import Post, Like
-from rest_framework import status
-from notifications.models import Notification
 
 
 # Create your views here.
-
-class LikePostView(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request):
-        pk = request.data.get('post_id')
-        try:
-            post = post = generics.get_object_or_404(Post, pk=pk)
-            like, created = Like.objects.get_or_create(post=post, liked_by = request.user)
-            if created:
-                Notification.objects.create(
-                    recipient=post.user,
-                    actor=request.user,   
-                    verb="liked your post", 
-                    target=post           
-                )
-                return Response({'detail':'Post liked successfully.'}, status=status.HTTP_200_OK)
-            return Response({'detail':'You can not like multiple times'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        except Post.DoesNotExist:
-            return Response({'detail':'Post not found'}, status=status.HTTP_404_NOT_FOUND)
-
-
-class UnlikePostView(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request):
-        post_id = request.data.get('post_id')
-        try:
-            post = Post.objects.get(id=post_id)
-            like = Like.objects.get(post=post, liked_by=request.user)
-            like.delete()
-            return Response({'message':'Post unliked succefully.'}, status=status.HTTP_200_OK)
-        except Like.DoesNotExist:
-            return Response({'message':'Like not found'}, status=status.HTTP_404_NOT_FOUND)
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,7 +3,10 @@ from rest_framework import viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.generics import ListAPIView
-from rest_framework import permissions
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from posts.models import Post, Like
+from rest_framework import status
 # Create your views here.
 
 
@@ -25,3 +28,34 @@ class FeedView(ListAPIView):
         user = self.request.user
         following_users = user.following.all()
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+
+
+class LikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        pk = request.data.get('post_id')
+        try:
+            post = post = generics.get_object_or_404(Post, pk=pk)
+            like, created = Like.objects.get_or_create(post=post, user=request.user)
+            if created:
+                return Response({'detail':'Post liked successfully.'}, status=status.HTTP_200_OK)
+            return Response({'detail':'You can not like multiple times'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Post.DoesNotExist:
+            return Response({'detail':'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UnlikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        post_id = request.data.get('post_id')
+        try:
+            post = Post.objects.get(id=post_id)
+            like = Like.objects.get(post=post, liked_by=request.user)
+            like.delete()
+            return Response({'message':'Post unliked succefully.'}, status=status.HTTP_200_OK)
+        except Like.DoesNotExist:
+            return Response({'message':'Like not found'}, status=status.HTTP_404_NOT_FOUND)
