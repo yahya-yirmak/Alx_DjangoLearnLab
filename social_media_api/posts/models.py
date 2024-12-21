204e3ba45aefd8b476f1aa14118c4e6fcb426687
yahya yirmak <yahyayirmak9@gmail.com>
2024-12-21 14:56:02 +0100

    Updating social_media_api

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -4,7 +4,7 @@ from accounts.models import CustomUser
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey(CustomUser, related_name='posts', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -22,7 +22,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    author = models.ForeignKey(CustomUser, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, related_name='comments', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
