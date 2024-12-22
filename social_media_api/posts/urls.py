5267f9c880b086b534213d1d80a85fcfb311ed70
yahya yirmak <yahyayirmak9@gmail.com>
2024-12-22 08:40:06 +0100

    Updating social_media_api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewset, CommentViewset
-from .views import FeedView
+from .views import FeedView, LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewset)
@@ -10,4 +10,6 @@ router.register(r'comments', CommentViewset)
 urlpatterns = [
     path('api/', include(router.urls)),
     path('feed/', FeedView.as_view(), name='feed'),
+    path('like/<str:post_id>', LikePostView.as_view(), name='like_post'),
+    path('unlike/<str:post_id>', UnlikePostView.as_view(), name='unlike_post'),
 ]
